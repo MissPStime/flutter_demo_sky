2012518eadf61ad7f6927baca1f041e3ff944856
zhulantian@adpanshi.com <112233qwE>
2020-07-24 15:17:07 +0800

    1.添加屏幕适配包 2.导航栏 3.抽屉菜单 4.底部导航

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
-# flutterappsky
-
-A new Flutter application.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. flutter_screenutil: ^1.1.0 #自适应屏幕管理
+2.scaffold是页面支架，可实现(appbar，抽屉菜单，底部导航)
--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutterappsky/find/find_page.dart';
+
+import 'home/home_page.dart';
+import 'me/me_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'MissPStime'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +33,162 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentIndex=1;
+  //底部导航对应的widget
+  List <Widget> _bodys=[HomePage(),FindPage(),MePage()];
 
-  void _incrementCounter() {
+  void _onBottomTap(int index){
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex=index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    ScreenUtil.init(context, width: 720, height: 1280, allowFontScaling: true);
     return Scaffold(
+      //导航栏
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(
+                Icons.apps,
+                color: Colors.white,
+              ),
+              //打开抽屉菜单
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.message,
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+      //抽屉菜单
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Colors.blue),
+              height: 200.h,
+              child: Center(
+                child: Text("drawer head"),
+              ),
+            ),
+            ListTile(
+              //设置内容边距，默认是 16，但我们在这里设置为 0
+              contentPadding:
+                  EdgeInsets.only(left: 10.w, top: 0, right: 10.w, bottom: 0),
+              //前置图片
+              leading: IconButton(
+                icon: Icon(
+                  Icons.phone,
+                ),
+              ),
+              title: Text("title"),
+              //副标题
+              subtitle: Text("subtitle"),
+              //dense，使字体更小
+              dense: false,
+              //后置图片
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              onLongPress: (){
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              //设置内容边距，默认是 16，但我们在这里设置为 0
+              contentPadding:
+              EdgeInsets.only(left: 10.w, top: 0, right: 10.w, bottom: 0),
+              //前置图片
+              leading: IconButton(
+                icon: Icon(
+                  Icons.phone,
+                ),
+              ),
+              title: Text("title"),
+              //副标题
+              subtitle: Text("subtitle"),
+              //dense，使字体更小
+              dense: false,
+              //后置图片
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              onLongPress: (){
+                Navigator.of(context).pop();
+              },
+              enabled: false,
+            ),
+            ListTile(
+              //选中，内容会变成主题色
+              selected: true,
+              //设置内容边距，默认是 16，但我们在这里设置为 0
+              contentPadding:
+              EdgeInsets.only(left: 10.w, top: 0, right: 10.w, bottom: 0),
+              //前置图片
+              leading: IconButton(
+                icon: Icon(
+                  Icons.phone,
+                ),
+              ),
+              title: Text("title"),
+              //副标题
+              subtitle: Text("subtitle"),
+              //dense，使字体更小
+              dense: false,
+              //后置图片
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              onLongPress: (){
+                Navigator.of(context).pop();
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //底部导航
+      bottomNavigationBar: BottomNavigationBar(
+        //底部导航配置
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home),title: Text("Home")),
+          BottomNavigationBarItem(icon: Icon(Icons.find_in_page),title: Text("Find")),
+          BottomNavigationBarItem(icon: Icon(Icons.account_box),title: Text("Me")),
+        ],
+        //当前选中的index
+        currentIndex: _currentIndex,
+        //点击事件
+        onTap: _onBottomTap,
+        //选中的颜色，默认为主题色
+        fixedColor: Colors.blue,
+        //底部导航栏的颜色
+        backgroundColor: Colors.white,
+      ),
+
+      body: _bodys[_currentIndex],
+     // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
